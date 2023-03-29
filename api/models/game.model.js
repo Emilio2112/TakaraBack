41b33a40bad7540a8ad4a360d4effdf98f969613
Emilio <emilio_casado_de_galdo@hotmail.com>
2023-03-29 13:27:21 +0100

    gameModel Fix

--- api/models/game.model.js
+++ api/models/game.model.js
@@ -1,6 +1,10 @@
 const mongoose = require('mongoose')
 
 const gameSchema = new mongoose.Schema({
+    id: {
+        type: Number,
+        unique: [true, "This game is registered"]
+    },
     name: {
         type: String,
         required: true
