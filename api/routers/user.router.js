cea2b445e54198e34a52dc5dfed2d7d4747bd7be
Emilio <emilio_casado_de_galdo@hotmail.com>
2023-04-04 11:44:06 +0100

    completed fixed

--- api/routers/user.router.js
+++ api/routers/user.router.js
@@ -15,7 +15,7 @@ router
   .get("/profile", authUser, getUserById)
   .get("/collection", authUser, getUserCollection)
   .get("/playing", authUser, getUserPlaying)
-  .get("/completd", authUser, getUserCompleted)
+  .get("/completed", authUser, getUserCompleted)
   .put("/profile", authUser, updateUser)
   .delete("/profile", authUser, deleteUserById)
   .patch("/favorite/add", authUser, addGameToCollection);
